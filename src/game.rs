d0a1bbcdbd133fd31837f9170ed2fba3cbe8839f
sham-404 <shamsujith4@gmail.com>
2026-04-12 17:25:40 +0530

    Added debug_assert in king_pos change for safety

--- src/game.rs
+++ src/game.rs
@@ -1,7 +1,7 @@
 use crate::board::Board;
 use crate::piece::{Move, MoveKind, Piece, PieceColor};
 
-#[derive(Debug, Clone, Copy)]
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
 pub enum Square {
     Empty,
     Occupied(Piece),
@@ -510,11 +510,13 @@ impl Game {
 
         // update king_pos if white king moved
         if self.king_pos.white == from {
+            debug_assert!(self.board.peek(to) == &Square::Occupied(Piece::King(PieceColor::White)));
             self.king_pos.white = to;
         }
 
         // update king_pos if black king moved
         if self.king_pos.black == from {
+            debug_assert!(self.board.peek(to) == &Square::Occupied(Piece::King(PieceColor::Black)));
             self.king_pos.black = to;
         }
 
@@ -557,11 +559,13 @@ impl Game {
 
         // update king_pos if white king moved
         if self.king_pos.white == mov.to {
+            debug_assert!(self.board.peek(mov.from) == &Square::Occupied(Piece::King(PieceColor::White)));
             self.king_pos.white = mov.from;
         }
 
         // update king_pos if black king moved
         if self.king_pos.black == mov.to {
+            debug_assert!(self.board.peek(mov.from) == &Square::Occupied(Piece::King(PieceColor::Black)));
             self.king_pos.black = mov.from;
         }
 
